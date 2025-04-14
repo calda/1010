bd8eb880cac1ca1161edd3071129f1f77cc2f688
Cal Stephens <cal@calstephens.tech>
2025-04-13 18:02:39 -0700

    Initial Commit

---
+++ TenTen/TenTenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenTenApp.swift
+//  TenTen
+//
+//  Created by Cal Stephens on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TenTenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
